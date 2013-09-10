42863bf550f66a0d7975e7da7afe22ea88f8e6be
Travis Gertz <trav@louderthan10.com>
2013-09-10 01:58:17 -0700

    Setup some base files

---
+++ source/vendor/polyfill.mix.js
@@ -0,0 +1,3 @@
+// Include your polyfills here to make a nice, single, minified polyfill file for old browsers
+
+// e.g.) @import "name.of.plugin.file.js";
---
+++ source/vendor/vendor.mix.js
@@ -0,0 +1,3 @@
+// Include your plugin scripts here to make a nice, single, minified plugin file
+
+// e.g.) @import "name.of.plugin.file.js";
