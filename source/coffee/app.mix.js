d06a9b1add2085d566dd6776661052265db91342
Travis Gertz <trav@louderthanten.com>
2015-07-14 11:52:42 -0700

    Update folder

--- source/coffee/app.mix.js
+++ source/coffee/app.mix.js
@@ -1,7 +1,3 @@
-@import '../../httpdocs/js/typography.js';
-@import '../../httpdocs/js/media.js';
-@import '../../httpdocs/js/drawers.js';
-@import '../../httpdocs/js/forms.js';
-@import '../../httpdocs/js/scroll.js';
-@import '../../httpdocs/js/tabs.js';
-@import '../../httpdocs/js/offcanvas.js';
+@import '../../public_html/js/typography.js';
+@import '../../public_html/js/media.js';
+@import '../../public_html/js/drawers.js';
--- source/vendor/vendor.mix.js
+++ source/vendor/vendor.mix.js
@@ -1,2 +1,1 @@
 @import 'jquery/jquery.fitvids.js';
-@import 'prism/prism.js';
